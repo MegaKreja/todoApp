41e30d17dfde0c2a8ae674ac2a845ece47ce86f0
MegaKreja <kreja95ue@gmail.com>
2018-10-21 17:18:08 +0200

    Add get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose.js");
 var { Todo } = require("./models/todo");
@@ -32,6 +33,24 @@ app.get("/todos", (req, res) => {
       res.status(400).send(e);
     }
   );
+});
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (todo) {
+        res.send({ todos });
+      } else {
+        res.send(404).send();
+      }
+    })
+    .catch(() => res.status(400).send());
 });
 
 app.listen(3000, () => {
