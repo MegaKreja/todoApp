e9713b2814ad02814d2e2a0ef76ef2c4cc562666
MegaKreja <kreja95ue@gmail.com>
2018-10-22 20:27:34 +0200

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -53,6 +53,23 @@ app.get("/todos/:id", (req, res) => {
     .catch(() => res.status(400).send());
 });
 
+app.delete("/todos/:id", (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        res.send(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(() => res.status(400).send());
+});
+
 app.listen(port, () => {
   console.log(`Starting on port ${port}`);
 });
